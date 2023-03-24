eb7ce836cf2ccf02dd17f7b841bd76f35b771ab9
zhejian w <zhejian.wang@gmail.com>
2023-03-23 22:00:25 -0500

    refresh rust syntax

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (apple_count: i32) -> i32{
+    let mut single_price: i32 = 2;
+    if( apple_count > 40) {
+        single_price = 1;
+    }
+    apple_count * single_price
+}
 
 // Don't modify this function!
 #[test]
